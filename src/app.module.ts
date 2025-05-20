bae9e96730968d1e5d8732c46efe2f1f4e081f5c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-20 16:23:55 +0600

    nest controler and service with get

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService],
+})
+export class AppModule {}
---
+++ src/product/product.service.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductService {
+    private products = [
+        { id:1, name:"Mobile",price:20000 },
+        { id:2, name:"tablet",price:20000 },
+        { id:3, name:"laptop",price:20000 },
+    ]
+
+    getAllProduct(){
+        return this.products;
+    }
+
+    getProductById(id:number){
+        return this.products.find((product)=> product.id === id)
+    }
+}
